33c4b6497dc727a11914a53bceb70dc861e6bc30
Fouad Nassif <110334322+FouadNassif@users.noreply.github.com>
2024-03-26 16:47:33 +0200

    fixing database and api errors for restaurant

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\Api\ReservationController;
+use App\Http\Controllers\Api\RestaurantController;
 
 /*
 |--------------------------------------------------------------------------
