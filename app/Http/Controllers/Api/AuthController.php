d2c2bcf42218d5ea4ba30ae3815257357c858d93
Fouad Nassif <110334322+FouadNassif@users.noreply.github.com>
2024-04-03 18:12:13 +0300

    :bug: Fixing the user can't reset the password

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -9,65 +9,57 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function signup(request $request)
+    public function signup(Request $request)
     {
-        //validate data from user
+        // validate data from user
         $request->validate([
             "Username" => "required|string",
             "email" => "required|email|unique:users",
-            "Phonenumber"=>"required|string",
+            "Phonenumber" => "required|string",
             "password" => "required|confirmed|min:6",
-           
         ]);
-        
-        //create new user and save it to database
+
+        // create new user and save it to database
         $user = User::create([
             'Username' => $request->Username,
             'email' => $request->email,
-            'Phonenumber'=>$request->Phonenumber,
+            'Phonenumber' => $request->Phonenumber,
             'password' => bcrypt($request->password),
-            'password_confirmation' => 'required|same:password'
-            ]);
-         $access_token= $user->createToken("authToken")->plainTextToken;  
-         if($user){
-            return response()->json([
-                'status'=>'201',
-                'message'=>'User has been created successfully.',
-                'token'=> $access_token
-                ], 201);
-        }else{
-            return response()->json([
-                'status'=>'404',
-                'message'=>'somethings was worng'
-                ], 404);
-        }
+        ]);
 
         // Authenticate the user
         Auth::login($user);
 
-        }
-    
-    public function login(Request $request){
-        //validate data from the user
-        $credentials=$request->validate([
+        $access_token = $user->createToken("authToken")->plainTextToken;
+
+        return response()->json([
+            'status' => '201',
+            'message' => 'User has been created successfully.',
+            'token' => $access_token
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        // validate data from the user
+        $credentials = $request->validate([
             'Username' => 'required|string',
             'password' => 'required|string|min:8',
         ]);
-       
-        if(Auth::attempt($credentials)){
-         $user=Auth::user();
-         $access_token =$user->createToken('authToken')->plainTextToken;
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $access_token = $user->createToken('authToken')->plainTextToken;
             return response()->json([
-                'success'=>true,
-                'message'=>'Logged In Successfully!',
-                 'token'=>$access_token
-                ],201);
-        }else{
+                'success' => true,
+                'message' => 'Logged In Successfully!',
+                'token' => $access_token
+            ], 201);
+        } else {
             return response()->json([
-                'status'=>200,
-                'message'=>'invalid  Email or Password'],200);
+                'status' => 200,
+                'message' => 'Invalid Email or Password'
+            ], 200);
         }
     }
-         
-    
 }
