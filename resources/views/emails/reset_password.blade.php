933e5915c7e0e4ac1f304ac4061192afe0a80ff2
Fouad Nassif <110334322+FouadNassif@users.noreply.github.com>
2024-04-03 18:06:21 +0300

    :memo: Added Email Template

---
+++ resources/views/emails/reset_password.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Reset</title>
+</head>
+<body>
+    <p>Hello,</p>
+    <p>You can reset your password by clicking the link below:</p>
+    <a href="{{ $resetUrl }}">Reset Password</a>
+    <p>If you did not request a password reset, no further action is required.</p>
+    <p>Thank you.</p>
+</body>
+</html>
