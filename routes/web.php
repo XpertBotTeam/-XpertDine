da89186dc3b4d449e89023b85e580d691fd24e5f
Fouad Nassif <110334322+FouadNassif@users.noreply.github.com>
2024-04-05 21:19:51 +0300

    :sparkles: Adding the sign/log in/out and restaurant menu and create a new form routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\RestaurantController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ResetPasswordController;
+use App\Http\Controllers\MenuController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,12 +16,32 @@ use App\Http\Controllers\Api\ResetPasswordController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+// Welcome Page
 Route::get('/', [RestaurantController::class, 'all']);
 
+// Single Restaurant Informations
 Route::get('/restaurant/{id}', [RestaurantController::class, 'single'])->name('restaurant.show');
 
+// Reset Password
 Route::get('/password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
 
+// Add a new Account
 Route::get('/register', [AuthController::class, 'signupForm']);
-
 Route::post('/signup', [AuthController::class, 'signup']);
+
+// Add a new Account
+Route::get('/login', [AuthController::class, 'loginForm']);
+Route::post('/loginCheck', [AuthController::class, 'login']);
+
+// Add a new Restaurant
+Route::get('/create/restaurant', [RestaurantController::class, 'show']);
+Route::post('/create/restaurantCheck', [RestaurantController::class, 'store']);
+
+// Add a new Menu Item
+Route::get('/create/menu', [MenuController::class, 'show']);
+Route::post('/create/menuCheck', [MenuController::class, 'check']);
+
+// Logout from account
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
