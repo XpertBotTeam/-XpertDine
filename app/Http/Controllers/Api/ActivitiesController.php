4cee11f36079e9b4fb59ed57a1f486e5aee616bf
Fouad Nassif <110334322+FouadNassif@users.noreply.github.com>
2024-04-18 21:46:58 +0300

    1. :sparkles: Add the activities table to the database

---
+++ app/Http/Controllers/Api/ActivitiesController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Activities;
+use Illuminate\Http\Request;
+use function Laravel\Prompts\error;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redirect;
+
+class ActivitiesController extends Controller
+{
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'Name' => 'required',
+            'Images' => 'required',
+            'Description' => 'required',
+            'Location' => 'required',
+            'PhoneNumber' => 'required'
+        ]);
+
+        $ImagesPath = null;
+        if ($request->hasFile('Images')) {
+            $ImagesPath = $request->file('Images')->store('logos', 'public');
+        }
+        $Activities = Activities::create([
+            'Name' => $request->Name,
+            'Images' => $ImagesPath,
+            'Description' => $request->Description,
+            'Location' => $request->Location,
+            'PhoneNumber' => $request->PhoneNumber
+        ]);
+
+        if ($request->header('User-Agent') === 'Flutter') {
+            return response()->json([
+                'status' => '201',
+                'data'=>$Activities,
+                'message' => 'Activities has been created successfully.'
+            ], 201);
+        } else {
+            return response()->json([
+                'status' => '201',
+                'data'=>$Activities,
+                'message' => 'Activities has been created successfully.'
+            ], 201);
+        }
+    }
+
+    public function destroy(string $id)
+    {
+        $Activities = Activities::findOrFail($id);
+        $Activities->delete();
+        return response()->json([
+            "status"=>true,
+            "message"=>'Deleted Successfully'
+        ]);
+        }
+}
